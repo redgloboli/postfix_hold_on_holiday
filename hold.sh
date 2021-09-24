a9beeecc46976e7f406257e5d34f501022834109
redgloboli <71544926+redgloboli@users.noreply.github.com>
2021-09-24 13:07:37 +0200

    Update hold.sh

--- hold.sh
+++ hold.sh
@@ -24,59 +24,70 @@ VDAYTILL=2021-04-01
 VTIMEFROM=0600
 VTIMETILL=2000
 
-if [[ -n $HOLIDAY || ($DAYOFWEEK -eq 7) ]]; then
-   # Holiday -> HOLD or Sunday = Holiday -> HOLD
+# Functions
+hold () {
    sed -i 's|[#,]||g' /etc/postfix/hold
    /usr/sbin/postmap hash:/etc/postfix/hold
+}
 
-elif [[ ($DAYOFWEEK -eq 1) && ! -n $HOLIDAY && "$NOW" < "$OFFTIME" ]]; then
-   # Monday and not holiday before 6 o'clock am -> HOLD
-   sed -i 's|[#,]||g' /etc/postfix/hold
+hold_off () {
+   sed -i '/^[^#]/ s/^/#/' /etc/postfix/hold
    /usr/sbin/postmap hash:/etc/postfix/hold
+   sleep 20
+   /usr/sbin/postsuper -r ALL
+}
 
-elif [[ ($VDATE == $VDAYFROM) && ($NOW > $VTIMEFROM) ]]; then
-   # Vacation -> redirect e-mail
+redirect () {
    grep -qsxF 'redirect "user@redirect.to";' /path/to/sieve-script/sieve/sogo.sieve || printf "redirect \"user@redirect.to\";\n" >> /path/to/sieve-script/sogo.sieve
    chown vmail:vmail /path/to/sieve-script/sieve/sogo.sieve
    chmod 600 /path/to/sieve-script/sieve/sogo.sieve
    ln -fs sieve-script/sogo.sieve /path/to/sieve-script/.dovecot.sieve
    sed -i 's|[#,]||g' /etc/postfix/hold
    /usr/sbin/postmap hash:/etc/postfix/hold
+}
 
-elif [[ ($VDATE > $VDAYFROM) && ($VDATE < $VDAYTILL) ]]; then
-   # Vacation -> HOLD
-   sed -i 's|[#,]||g' /etc/postfix/hold
-   /usr/sbin/postmap hash:/etc/postfix/hold
-
-elif [[ ($VDATE == $VDAYTILL) && ($NOW < $VTIMETILL) ]]; then
-   # Vacation -> HOLD
-   sed -i 's|[#,]||g' /etc/postfix/hold
-   /usr/sbin/postmap hash:/etc/postfix/hold
-
-elif [[ ($VDATE == $VDAYTILL) && ($NOW > $VTIMETILL) ]]; then
-   # End of vacation -> undo redirect -> HOLD OFF
+redirect_off () {
    sed -i '/redirect "user@redirect.to";/d' /path/to/sieve-script/sieve/sogo.sieve
    sed -i '/^[^#]/ s/^/#/' /etc/postfix/hold
    /usr/sbin/postmap hash:/etc/postfix/hold
    sleep 20
    /usr/sbin/postsuper -r ALL
+}
+
+
+if [[ -n $HOLIDAY || ($DAYOFWEEK -eq 7) ]]; then
+   # Holiday -> HOLD or Sunday = Holiday -> HOLD
+   hold
+
+elif [[ ($DAYOFWEEK -eq 1) && ! -n $HOLIDAY && "$NOW" < "$OFFTIME" ]]; then
+   # Monday and not holiday before 6 o'clock am -> HOLD
+   hold
+
+elif [[ ($VDATE == $VDAYFROM) && ($NOW > $VTIMEFROM) ]]; then
+   # Vacation -> redirect e-mail
+   redirect
+
+elif [[ ($VDATE > $VDAYFROM) && ($VDATE < $VDAYTILL) ]]; then
+   # Vacation -> HOLD
+   hold
+
+elif [[ ($VDATE == $VDAYTILL) && ($NOW < $VTIMETILL) ]]; then
+   # Vacation -> HOLD
+   hold
+
+elif [[ ($VDATE == $VDAYTILL) && ($NOW > $VTIMETILL) ]]; then
+   # End of vacation -> undo redirect -> HOLD OFF
+   redirect_off
 
 elif [[ ($DAYOFWEEK -eq 1) && ! -n $HOLIDAY && "$NOW" > "$OFFTIME" ]]; then
    # Monday and not holiday from 6 o'clock am -> HOLD OFF
-   sed -i '/^[^#]/ s/^/#/' /etc/postfix/hold
-   /usr/sbin/postmap hash:/etc/postfix/hold
-   sleep 20
-   /usr/sbin/postsuper -r ALL
+   hold_off
 
 elif [[ ($DAYOFWEEK -eq 6) && ! -n $HOLIDAY && "$NOW" > "$ONTIME" ]]; then
    # Saturday and not holiday from 1 o'clock pm -> HOLD
-   sed -i 's|[#,]||g' /etc/postfix/hold
-   /usr/sbin/postmap hash:/etc/postfix/hold
+   hold
 
 elif [[ ! -n $HOLIDAY ]]; then
    # not holiday  -> HOLD OFF
-   sed -i '/^[^#]/ s/^/#/' /etc/postfix/hold
-   /usr/sbin/postmap hash:/etc/postfix/hold
-   sleep 20
-   /usr/sbin/postsuper -r ALL
+   hold_off
 fi
